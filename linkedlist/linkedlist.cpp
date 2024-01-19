ca6a7967c9d5484b56b5eec1cc882bd48002d9b2
buddhadeb <jeetkoner36@gmail.com>
2024-01-19 17:14:12 +0530

    insert funtction in linkedlist

--- linkedlist/linkedlist.cpp
+++ linkedlist/linkedlist.cpp
@@ -17,6 +17,7 @@ Write a menu driven C program to Perform the following operations on Linked List
 #include <stdio.h>
 #include <stdlib.h>
 
+void MenuDrivenProgram();
 void Insertappend();
 void InsertBeg();
 void InsertNThposition();
@@ -36,46 +37,8 @@ struct node *root = NULL;
 
 int main()
 {
-    int choice;
-    while (1)
-    {
-        printf("Press 1 to at insert begining\nPress 2 at insert end (append)\nPress 3 to insert at n-th position\n");
-        printf("Press 4 Delete from Bgenning\npress 5 Delete from end\npress 6 Delete From n th position\n");
-        printf("Press 7 to display\nPress 8 to Count\nPress 9 to exit\n");
-        scanf("%d", &choice);
+    MenuDrivenProgram();
 
-        switch (choice)
-        {
-        case 1:
-            InsertBeg();
-            break;
-        case 2:
-            Insertappend();
-            break;
-        case 3:
-            InsertNThposition();
-            break;
-        case 4:
-            DeleteBeg();
-            break;
-        case 5:
-            DeleteEnd();
-            break;
-        case 6:
-            DeleteNthPosition();
-            break;
-        case 7:
-            display();
-            break;
-        case 8:
-            Count();
-            break;
-        case 9:
-            exit(0);
-        default:
-            exit(0);
-        }
-    }
     return 0;
 }
 
@@ -139,6 +102,7 @@ int Count()
     if (root == NULL)
     {
         printf("\nList is empty.\n");
+        MenuDrivenProgram();
     }
 
     p = root;
@@ -146,7 +110,6 @@ int Count()
     {
         count++;
         p = p->link;
-        printf("");
     }
     printf("\nNumber of elements in the list: %d\n", count);
     return count;
@@ -154,9 +117,55 @@ int Count()
 
 void InsertNThposition()
 {
-    
-}
+    int position;
+    printf("\nEnter the Position Number Where you want to insert:\n");
+    scanf("%d", &position);
 
+    if (position > 0)
+    {
+        struct node *temp, *p;
+        int n;
+
+        temp = (struct node *)malloc(sizeof(struct node));
+
+        printf("\nEnter data to be inserted:\n");
+        scanf("%d", &n);
+
+        temp->data = n;
+        temp->link = NULL;
+
+        if (position == 1)
+        {
+            temp->link = root;
+            root = temp;
+            printf("\nData inserted successfully at position %d\n", position);
+        }
+        else
+        {
+            p = root;
+
+            for (int i = 1; (i < position - 1 && p != NULL); i++)
+            {
+                p = p->link;
+            }
+
+            if (p != NULL)
+            {
+                temp->link = p->link;
+                p->link = temp;
+                printf("\nData inserted successfully at position %d\n", position);
+            }
+            else
+            {
+                printf("\nInvalid position. Insertion failed.\n");
+            }
+        }
+    }
+    else
+    {
+        printf("\nInvalid position. Insertion failed.\n");
+    }
+}
 
 void DeleteBeg()
 {
@@ -185,5 +194,49 @@ void display()
             p = p->link;
         }
         printf("\n");
+    }
+}
+
+void MenuDrivenProgram()
+{
+    int choice;
+    while (1)
+    {
+        printf("\n\n\nPress 1 to at insert begining\nPress 2 at insert end (append)\nPress 3 to insert at n-th position\n");
+        printf("Press 4 Delete from Bgenning\npress 5 Delete from end\npress 6 Delete From n th position\n");
+        printf("Press 7 to display\nPress 8 to Count\nPress 9 to exit\n~ ");
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+        case 1:
+            InsertBeg();
+            break;
+        case 2:
+            Insertappend();
+            break;
+        case 3:
+            InsertNThposition();
+            break;
+        case 4:
+            DeleteBeg();
+            break;
+        case 5:
+            DeleteEnd();
+            break;
+        case 6:
+            DeleteNthPosition();
+            break;
+        case 7:
+            display();
+            break;
+        case 8:
+            Count();
+            break;
+        case 9:
+            exit(0);
+        default:
+            exit(0);
+        }
     }
 }
